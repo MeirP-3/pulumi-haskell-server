904b1444bef597419bd7ba6aa7ceeb7c0eab1886
Ubuntu <ubuntu@ip-172-31-47-45.ec2.internal>
2020-05-10 00:42:52 +0000

    change name and reduce count to 1

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import * as awsx from '@pulumi/awsx';
 
 // Create a load balancer to listen for requests and route them to the container.
-let lb = new awsx.lb.NetworkListener('nginx', { port: 80 });
-let service = new awsx.ecs.FargateService('nginx', {
-    desiredCount: 2,
+let lb = new awsx.lb.NetworkListener('haskell', { port: 80 });
+let service = new awsx.ecs.FargateService('haskell', {
+    desiredCount: 1,
     taskDefinitionArgs: {
         containers: {
-            nginx: {
-                image: awsx.ecs.Image.fromPath('nginx', './app'),
+            haskell: {
+                image: awsx.ecs.Image.fromPath('haskell', './app'),
                 memory: 512,
                 portMappings: [ lb ]
             }
